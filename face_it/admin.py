2cd8df10d4d57a76d4947a0e584379c1fe3de3f0
Sean Lewis <sean.lewis@excella.com>
2015-03-13 15:28:56 -0400

    Implemented csv export functionality in the admin page

--- face_it/admin.py
+++ face_it/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
 from core.models import UserMetrics, UserProfile, GlobalMetrics, MostKnown
 
 
-admin.site.register(UserMetrics)
-admin.site.register(UserProfile)
-admin.site.register(GlobalMetrics)
-admin.site.register(MostKnown)
+class UserMetricsResource(resources.ModelResource):
+
+    class Meta:
+        model = UserMetrics
+
+
+class GlobalMetricsResource(resources.ModelResource):
+    class Meta:
+        model = GlobalMetrics
+
+
+class MostKnownResource(resources.ModelResource):
+    class Meta:
+        model = MostKnown
+
+
+class UserMetricsAdmin(ImportExportActionModelAdmin):
+    resource_class = UserMetricsResource
+
+
+class GlobalMetricsAdmin(ImportExportActionModelAdmin):
+    resource_class = GlobalMetrics
+
+
+class MostKnownAdmin(ImportExportActionModelAdmin):
+    resource_class = MostKnown
+
+
+admin.site.register(UserProfile),
+admin.site.register(GlobalMetrics, GlobalMetricsAdmin)
+admin.site.register(MostKnown, MostKnownAdmin)
+admin.site.register(UserMetrics, UserMetricsAdmin)
