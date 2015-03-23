06130817c824c0544b8618ae496e12a4b38f41c5
etapau <etapau@gmail.com>
2015-03-23 16:57:55 -0400

    Heroku Deployment & RedisToGo configuration

--- face_it/settings/production.py
+++ face_it/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:@localhost:5432/db')}
+DATABASES = {'default': dj_database_url.config(default='postgres://waxqvtkhzjuakg:6M5hafAUkFzrgEz2KCjBT065Ih@ec2-107-22-253-198.compute-1.amazonaws.com:5432/d93akfi9778vpr')}
 
 try:
 	from .local import *
