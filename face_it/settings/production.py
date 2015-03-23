97a720acbc9f38b797c1ed24867e916b4a5c7bdb
etapau <etapau@gmail.com>
2015-03-23 16:43:47 -0400

    Heroku Deployment & RedisToGo configuration

--- face_it/settings/production.py
+++ face_it/settings/production.py
@@ -2,7 +2,13 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['*']
+
 try:
 	from .local import *
 except ImportError:
 	pass
+
+
+SOCIAL_AUTH_YAMMER_KEY = 'R97DK2ADollIPpQZz4eeA'
+SOCIAL_AUTH_YAMMER_SECRET = 'lLtRosuTAso9PQWDojdrhWtSvNk2UZIGbAREjhT2Qg'
