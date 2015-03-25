fd03c80eb7b705f6f3e9e6c554950ed15a7ecdd4
Mike Brown <mike.brown@excella.com>
2015-03-25 06:57:57 -0400

    Remove yammer api key that was accidentally committed

--- face_it/settings/dev.py
+++ face_it/settings/dev.py
@@ -6,6 +6,3 @@ try:
 	from .local import *
 except ImportError:
 	pass
-
-SOCIAL_AUTH_YAMMER_KEY = 'OCnfR7VJNBbcSS8GXMCi3A'
-SOCIAL_AUTH_YAMMER_SECRET = 'y1MZSoo0MuX8RJPEjMbZWJDvafR9mZmFWWUfHOcZgxM'
