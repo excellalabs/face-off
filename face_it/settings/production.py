85bd6e1e7e15a1a0a4e77c05928057edd2d2221f
etapau <etapau@gmail.com>
2015-03-26 15:38:41 -0400

    removed Django pipeline to feed static assets through whitenoise instead. Also removed memcache references

--- face_it/settings/production.py
+++ face_it/settings/production.py
@@ -1,17 +1,16 @@
 from .base import *
 import dj_database_url
 
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('HEROKU_PSTGRS_URL'))}
-
 try:
 	from .local import *
 except ImportError:
 	pass
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {'default': dj_database_url.config()}
 
 SOCIAL_AUTH_YAMMER_KEY = os.environ.get('SOCIAL_AUTH_YAMMER_KEY')
 SOCIAL_AUTH_YAMMER_SECRET = os.environ.get('SOCIAL_AUTH_YAMMER_SECRET')
