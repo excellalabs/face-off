9d6c477402554c52b78b8753f9794bd1bc9cf5ce
etapau <etapau@gmail.com>
2015-04-06 17:53:03 -0400

    minified version of util.js (manual)

--- core/static/js/util.js
+++ core/static/js/util.js
@@ -1,15 +1,1 @@
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-function formlessAjaxPostCSRFSetup() {
-    var csrftoken = $.cookie('csrftoken');
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-}
+function csrfSafeMethod(e){return/^(GET|HEAD|OPTIONS|TRACE)$/.test(e)}function formlessAjaxPostCSRFSetup(){var e=$.cookie("csrftoken");$.ajaxSetup({beforeSend:function(t,o){csrfSafeMethod(o.type)||this.crossDomain||t.setRequestHeader("X-CSRFToken",e)}})}
