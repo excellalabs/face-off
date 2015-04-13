c1925762931f0a5dc378f7b274044dca4a240b2e
etapau <etapau@gmail.com>
2015-04-13 12:24:51 -0400

    card flip test

--- core/features/environment.py
+++ core/features/environment.py
@@ -12,15 +12,17 @@ def before_all(context):
 
     setup_debug_on_error(context.config.userdata)
 
-    desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
-    desired_capabilities['version'] = '12'
-    desired_capabilities['platform'] = 'WINDOWS'
-    desired_capabilities['name'] = 'Testing Col-League App'
-    desired_capabilities['client_key'] = '44fa53ac66807797ca8ec3f1e9f9a9d9'
-    desired_capabilities['client_secret'] = '5cc0cce8a93b22b347ffd72204d007bb'
+    context.browser = webdriver.Firefox()
 
-    context.browser = webdriver.Remote(desired_capabilities=desired_capabilities,
-                                       command_executor="http://hub.testingbot.com:4444/wd/hub")
+    # desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
+    # desired_capabilities['version'] = '12'
+    # desired_capabilities['platform'] = 'WINDOWS'
+    # desired_capabilities['name'] = 'Testing Col-League App'
+    # desired_capabilities['client_key'] = '44fa53ac66807797ca8ec3f1e9f9a9d9'
+    # desired_capabilities['client_secret'] = '5cc0cce8a93b22b347ffd72204d007bb'
+    # webdriver.Remote(desired_capabilities=desired_capabilities, command_executor="http://hub.testingbot.com:4444/wd/hub")
+
+
 
 def after_step(context, step):
     if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
--- core/features/steps/steps.py
+++ core/features/steps/steps.py
@@ -1,7 +1,54 @@
-@when('we visit col-league')
-def step(context):
-    context.browser.get('http://col-league.herokuapp.com')
+from behave import given, when, then
 
-@then('it should have the title "Sign In"')
+
+@given('the user is at the Face Off application')
 def step(context):
-    assert context.browser.title == "Sign In"
+    context.browser.get('http://localhost:8000')
+
+
+@given('the user has logged into the Face Off application')
+def step(context):
+    context.browser.get('http://localhost:8000')
+    login(context, 'emmanuel.apau@excella.com', 'ForWork12')
+
+
+@given('they are in practice mode')
+def step(context):
+    context.browser.find_element_by_id('easyGame').click()
+
+
+@when('the user clicks the first card')
+def step(context):
+    click_card(context.browser, 0)
+
+
+@when('they sign into the application through yammer')
+def step(context):
+    login(context, 'emmanuel.apau@excella.com', 'ForWork12')
+
+
+@then('they should arrive at the begin game screen')
+def step(context):
+    assert 'Face Off' in context.browser.title
+
+
+@then('all the cards should flip')
+def step(context):
+    for i in range(0, 4):
+        assert "clickFlip" in get_card(context.browser, i).get_attribute("class")
+
+
+# Helper Methods
+def login(context, username, password):
+    context.browser.find_element_by_name('signIn').click()
+    context.browser.find_element_by_name('login').send_keys(username)
+    context.browser.find_element_by_name('password').send_keys(password)
+    context.browser.find_element_by_class_name('yj-btn').click()
+
+
+def click_card(driver, index):
+    driver.find_element_by_id('colleague' + str(index)).click()
+
+
+def get_card(driver, index):
+    return driver.find_element_by_id('colleague' + str(index))
