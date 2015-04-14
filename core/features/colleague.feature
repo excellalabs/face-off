d422330cb3c968ee09e37c70697217d0fd4276d3
Sean Lewis <sean.lewis@excella.com>
2015-04-14 10:18:55 -0400

    Adding selenium test

--- core/features/colleague.feature
+++ core/features/colleague.feature
@@ -4,3 +4,9 @@ Scenario: visit col-league and check
   Given the user is at the Face Off application
   When they sign into the application through yammer
   Then they should arrive at the begin game screen
+
+Scenario: check educational mode
+  Given the user has logged into the Face Off application
+  When they are in practice mode
+  And the user clicks the first card
+  Then all the cards should flip
--- core/features/steps/steps.py
+++ core/features/steps/steps.py
@@ -9,12 +9,16 @@ def step(context):
 @given('the user has logged into the Face Off application')
 def step(context):
     context.browser.get('http://localhost:8000')
-    login(context, 'emmanuel.apau@excella.com', 'ForWork12')
+    login(context, 'sean.lewis@excella.com', 'Excella2014')
 
 
 @given('they are in practice mode')
 def step(context):
     context.browser.find_element_by_id('easyGame').click()
+
+@given('they are in competitive mode')
+def step(context):
+    context.browser.find_element_by_id('hardGame').click()
 
 
 @when('the user clicks the first card')
@@ -24,14 +28,17 @@ def step(context):
 
 @when('they sign into the application through yammer')
 def step(context):
-    login(context, '@excella.com', '')
-
+    login(context, 'sean.lewis@excella.com', 'Excella2014')
 
 
 @then('they should arrive at the begin game screen')
 def step(context):
     assert 'Face Off' in context.browser.title
 
+@then('all the cards should not flip')
+def step(context):
+    for i in range(0,4):
+        assert "greyOut" in get_card(context.browser, i).get_attribute("class")
 
 @then('all the cards should flip')
 def step(context):
