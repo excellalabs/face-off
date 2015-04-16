3b4aca5dd7b3556178561221a9e70f7375775be5
etapau <etapau@gmail.com>
2015-04-16 13:33:12 -0400

    Fixed bug with duplicate users in Global Ten metric, general refactoring of view code for readability

--- core/views.py
+++ core/views.py
@@ -27,39 +27,13 @@ def cards(request):
         network = 'default'
 
     if not redis_con.exists(network + '_users'):
-        users = []
-        social = request.user.social_auth.get(provider='yammer')
-        access_token = social.extra_data['access_token']
-
-        page = 1
-
-        while True:
-            response = requests.get(
-                'https://www.yammer.com/api/v1/users.json?page=%d' % page,
-                headers={'Authorization': 'Bearer %s' % access_token['token']}
-            )
-            if response.json() == []:
-                break
-            users.extend(response.json())
-            page += 1
-
-        pattern = re.compile('.+no_photo.png$')  # Filters out users with no photo
-        for user in users:
-            if not pattern.match(user['mugshot_url']):
-                redis_con.sadd(user['network_name'] + '_users', {
-                    'id': user['id'],
-                    'name': user['full_name'],
-                    'mugshot': user['mugshot_url_template'],
-                    'user_url': user['web_url'],
-                    'network': user['network_name'],
-                })
-        redis_con.expire(network + '_users', 43200)  # Redis cache expiration set to 12hrs(43200s)
+        users = yammer_user_restcall(request)
+        load_users_to_cache(redis_con, users, network)
 
     user_round_matrix = [four_random_cards(redis_con, network) for x in range(5)]
     answer = random.choice(user_round_matrix[0])
 
-    context = RequestContext(request, {'cards': user_round_matrix, 'answer': answer,
-                                       'round': 0, 'score': 0})
+    context = RequestContext(request, {'cards': user_round_matrix, 'answer': answer, 'round': 0, 'score': 0})
     return render_to_response('game.html', context_instance=context)
 
 
@@ -78,20 +52,7 @@ def next_round(request):
 
     # Prepares data for next round
     round += 1
-    answer = random.choice(card_matrix[round])
-    unique_answer = False
-
-    # Checks for previously used answer
-    while not unique_answer:
-        check_again = False
-        for i in range(0, round):
-            if card_matrix[i]['id'] == answer['id']:
-                answer = random.choice(card_matrix[round])
-                check_again = True
-                break
-
-        if not check_again:
-            unique_answer = True
+    answer = filter_previously_used_answer(card_matrix, round)
 
     context = RequestContext(request, {'cards': card_matrix, 'round': round, 'answer': answer,
                                        'score': score, 'resultsForm': ResultForm()})
@@ -101,11 +62,7 @@ def next_round(request):
 
 @login_required
 def results(request):
-    try:
-        metric = UserMetrics.objects.get(user=request.user)
-    except ObjectDoesNotExist:
-        # This is here purely for backwards compatibility TODO remember to delete this line later
-        metric = UserMetrics.objects.create(user=request.user).save()
+    metric = UserMetrics.objects.get(user=request.user)
 
     if request.method == 'POST':
         form = ResultForm(request.POST)
@@ -126,37 +83,89 @@ def results(request):
 
 @login_required
 def metrics(request):
-    leastKnown = ColleagueGraph.objects.order_by('-times_incorrect').filter(user=request.user)[:5]
+    # Colleagues you should get to know
+    most_incorrect_colleagues = ColleagueGraph.objects.order_by('-times_incorrect').filter(user=request.user)[:5]
 
-    metrics = ColleagueGraph.objects.filter(user=request.user)
-    correct_arr = []
-    incorrect_arr = []
-    for metric in metrics:
-        correct_arr.append({"label": str(metric.name), "value": metric.times_correct})
-        incorrect_arr.append({"label": str(metric.name), "value": (metric.times_incorrect * -1)})
+    # Global Known Colleagues
+    globally_known = globally_known_colleagues()
+    names = []
+    times_correct = []
 
-    gMetrics = globallyKnownColleagues()
-    gNames = []
-    gKnown = []
-    gImgs = []
-
-    for metric in gMetrics:
-        gNames.append(str(metric.name))
-        gKnown.append(metric.times_correct)
-        gImgs.append(str(metric.img_url))
+    for metric in globally_known:
+        names.append(str(metric.name))
+        times_correct.append(metric.times_correct)
 
     # Colleagues Encountered Data
+    encountered = ColleagueGraph.objects.filter(user=request.user)
+    correct_arr = []
+    incorrect_arr = []
+    for colleague in encountered:
+        correct_arr.append({"label": str(colleague.name), "value": colleague.times_correct})
+        incorrect_arr.append({"label": str(colleague.name), "value": (colleague.times_incorrect * -1)})
+
     times_known = {"key": "Guessed Correctly", "color": 'red', "values": correct_arr}
     times_not_known = {"key": "Guessed Incorrectly", "color": "blue", "values": incorrect_arr}
     jsondata = [times_known, times_not_known]
 
-    context = RequestContext(request, {'gNames': gNames, 'gKnown': gKnown, 'gMugs': gImgs,
-                                       'leastknown': leastKnown, 'json': json.dumps(jsondata)
+    context = RequestContext(request, {'names': names, 'times_correct': times_correct,
+                                       'leastknown': most_incorrect_colleagues, 'data': json.dumps(jsondata)
                                        })
+
     return render_to_response('metrics.html', context_instance=context)
 
 
 # Helper Functions
+def yammer_user_restcall(request):
+    users = []
+    social = request.user.social_auth.get(provider='yammer')
+    access_token = social.extra_data['access_token']
+
+    page = 1
+
+    while True:
+        response = requests.get(
+            'https://www.yammer.com/api/v1/users.json?page=%d' % page,
+            headers={'Authorization': 'Bearer %s' % access_token['token']}
+        )
+        if response.json() == []:
+            break
+        users.extend(response.json())
+        page += 1
+
+    return users
+
+
+def load_users_to_cache(redis_con, users, network):
+    pattern = re.compile('.+no_photo.png$')  # Filters out users with no photo
+    for user in users:
+        if not pattern.match(user['mugshot_url']):
+            redis_con.sadd(user['network_name'] + '_users', {
+                'id': user['id'],
+                'name': user['full_name'],
+                'mugshot': user['mugshot_url_template'],
+                'user_url': user['web_url'],
+                'network': user['network_name'],
+            })
+    redis_con.expire(network + '_users', 43200)  # Redis cache expiration set to 12hrs(43200s)
+
+
+def filter_previously_used_answer(card_matrix, round):
+    answer = random.choice(card_matrix[round])
+    unique_answer = False
+    # Checks for previously used answer
+    while not unique_answer:
+        check_again = False
+        for i in range(0, round):
+            if card_matrix[i]['id'] == answer['id']:
+                answer = random.choice(card_matrix[round])
+                check_again = True
+                break
+
+        if not check_again:
+            unique_answer = True
+
+    return answer
+
 def save_metric_results(results, user):
     for result in results:
         if result:
@@ -190,11 +199,10 @@ def four_random_cards(redis_con, network):
     return [ast.literal_eval(person) for person in redis_con.srandmember(network + '_users', 4)]
 
 
-def globallyKnownColleagues():
-    top_scores = (ColleagueGraph.objects.order_by('-times_correct').values_list('times_correct', flat=True).distinct())
-    top_10_known_colleagues = (ColleagueGraph.objects.order_by('-times_correct').filter(
-        times_correct__in=top_scores[:10]))[:10]
-
+def globally_known_colleagues():
+    top_scores = (ColleagueGraph.objects.order_by('-times_correct').values_list('times_correct', flat=True).distinct())[:10]
+    filtered_by_name = (ColleagueGraph.objects.order_by('name','-times_correct').filter(times_correct__in=top_scores)).distinct('name')
+    top_10_known_colleagues = sorted(filtered_by_name, key=lambda ColleagueGraph:ColleagueGraph.times_correct, reverse=True)[:10]
     return top_10_known_colleagues
 
 
