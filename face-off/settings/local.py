d5de6051eeca6f6399bd5eef0ce2a666e10140bb
Sean Lewis <sean.lewis@excella.com>
2015-04-20 14:12:40 -0400

    removed local settings file

--- face-off/settings/local.py
+++
@@ -1,9 +0,0 @@
-DEBUG = True
-
-try:
-	from .base import *
-except ImportError:
-	pass
-
-SOCIAL_AUTH_YAMMER_KEY = 'jIaOdnxtu2Hihs1yU8Kw'
-SOCIAL_AUTH_YAMMER_SECRET = 'eLi0V9wwNhEqXyjMJCXJtXbZeDdsf78wV26N2Cm8D2o'
