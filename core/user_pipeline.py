4a6c8aed6e2424301510eb4492b772f78d81d6a8
etapau <etapau@gmail.com>
2015-04-22 13:20:06 -0400

    check/update user profile yammer url as they log in

--- core/user_pipeline.py
+++ core/user_pipeline.py
@@ -1,4 +1,6 @@
-from core.models import UserMetrics
+from core.models import UserMetrics, UserProfile
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def post_user_creation(backend, user, response, *args, **kwargs):
     if backend.name == 'yammer' and not user.yammer_url:
@@ -7,3 +9,12 @@ def post_user_creation(backend, user, response, *args, **kwargs):
         user.network = response['user']['network_name']
         user.save()
         UserMetrics.objects.create(user=user).save()
+    else:
+        try:
+            user = UserProfile.objects.get(email=response['user']['email'])
+            if user.yammer_url != response['user']['mugshot_url_template']:
+                user.yammer_url = response['user']['mugshot_url_template']
+                user.save()
+        except ObjectDoesNotExist:
+            return
+
