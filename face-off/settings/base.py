0b9e7baf30e625da684e9dd109b845b0015901c7
Sean Lewis <sean.lewis@excella.com>
2015-05-08 14:05:26 -0400

    added email values as environment values

--- face-off/settings/base.py
+++ face-off/settings/base.py
@@ -12,12 +12,12 @@ TEMPLATE_DEBUG = DEBUG
 PIPELINE_ENABLED = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'excellaconsultingfaceoff@gmail.com'
-EMAIL_HOST_PASSWORD = 'Excellaadmin'
-EMAIL_PORT = 587
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ADMINS = (
